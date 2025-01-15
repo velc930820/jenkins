62796ab992857da83e6a45de100072d91e7a44ff
carlos <velc930820@gmail.com>
2025-01-15 17:46:00 -0600

    jenkins 2

--- src/main/java/com/jenkinsapp/app/controller/HelloController.java
+++ src/main/java/com/jenkinsapp/app/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     //Este es un nuevo comentario
-    //Agregamos algo nuevo 2
+    //Agregamos algo nuevo 2.o 
     @GetMapping("/hello")
     public String saludar(){
         return "Hola Mundo con REST en Spring Boot";
